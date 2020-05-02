a27e605f775ac1b94a859e0824c2002db4f0c104
Toyin Dawudu <toyindawudu@gmail.com>
2020-05-02 02:11:35 +0100

    We can now filter through the task event list in the application

--- Tasklist/app.js
+++ Tasklist/app.js
@@ -11,32 +11,89 @@ loadEventListeners();
 
 // Function for all event listeners
 function loadEventListeners(){
+
   // Function for adding tasks
+
     form.addEventListener('submit', addTask);
+
+  // Remove the task event
+
+    taskList.addEventListener('click', removeTask);
+
+  // Clear task event list 
+
+    clearBtn.addEventListener('click', clearTasks);
+
+  // Filter through task list event
+
+    filter.addEventListener('keyup', filterTasks);
+
 }
 // Create a li element for the added task
 function addTask(e){
   if(taskInput.value === '') {
     alert('The task input is empty, so add a task.')
   }
-  // prevent default 
-  e.preventDefault();
-}
-  // Add a ul of collection
 
-  // Create text node and append to the li
+  // Add a li and an anchor link
+  const li = document.createElement('li');
+  const link = document.createElement('a');
+  
+  // Create text node and append to the li element
+  
+  li.appendChild(document.createTextNode(taskInput.value));
 
-  // Create a new link element 
+  // Add a classname to the anchor link 
 
-  // Add a delete class 
+  link.className = 'delete-item secondary-content';
 
   // Add icons to the Html to the link 
 
+  link.innerHTML = '<i class="fa fa-remove"></i>'
+
   // Append the link to the li
+  
+  li.appendChild(link);
 
-  // Append the li to the ul to show in the UI
-
+  // Append the li to the ul, so that it shows in the UI
+  
+  taskList.appendChild(li);
 
   // Clear the input
 
+  taskInput.value = '';
 
+  // console.log(li);
+
+  e.preventDefault();
+
+}
+
+// Remove Task 
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains('delete-item')){
+    e.target.parentElement.parentElement.remove();
+  }
+}
+
+// Clear Task List
+function clearTasks() {
+  // taskList.innerHTML = '';
+  while (taskList.firstChild){
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+// Filter Task List
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task) {
+    const item = item.firstChild.textContent;
+    if ( item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
+}
