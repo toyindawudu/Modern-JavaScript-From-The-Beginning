88f102df0a8408a118f8d9c18e8d165842ed99f0
Toyin Dawudu <toyindawudu@gmail.com>
2020-10-25 15:33:03 +0000

    Need to go over traversing the DOM content again

--- Lessons from course/lessons.js
+++ Lessons from course/lessons.js
@@ -7,29 +7,27 @@
 //   console.log('Hello');
 // }
 
-const sayHello = (name) => {
-  console.log(name)  // This prints out the name to the console when it's called 
-}
+// const sayHello = (name) => {
+//   console.log(name)  // This prints out the name to the console when it's called 
+// }
 
-const userName = (firstname, lastName) => {
-  console.log(`${firstname} ${lastName} welcome to the NBA`);
-}
+// const userName = (firstname, lastName) => {
+//   console.log(`${firstname} ${lastName} welcome to the NBA`);
+// }
 
 // sayHello('Ashley');  
 
 // userName('Josiah', 'Dawudu');
 
 // Using arrow functions as callbacks 
-const users = ['Toyin', 'Titi', 'Josiah'];
+// const users = ['Toyin', 'Titi', 'Josiah'];
 
-const nameLengths = users.map((name) => {
-  return name.length;
-});
+// const nameLengths = users.map((name) => {
+//   return name.length;
+// });
 
-let fruits = ["Pear", "Apple", "orange"];
+// let fruits = ["Pear", "Apple", "orange"];
 
-fruits.forEach(function(val){
-  console.log(val);
-})
-
-console.log(users);
+// fruits.forEach(function(val){
+//   console.log(val);
+// })
